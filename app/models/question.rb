64895bfcd9ecd13bb02b0b252f445b630a76e8a2
rodrigo-ehresmann <igoehresmann@gmail.com>
2017-05-27 11:18:22 -0300

    Implemented tests to question model

--- app/models/question.rb
+++ app/models/question.rb
@@ -3,4 +3,18 @@ class Question < ActiveRecord::Base
   has_many :answers
 
   validates_presence_of :description
+
+  before_destroy { answers.clear }
+
+  def add_answer(answer)
+    self.answers << answer
+  end
+
+  def add_tag(tag)
+    self.tags << tag
+  end
+
+  def should_be_destroyed?
+    answers.count == 0
+  end
 end
