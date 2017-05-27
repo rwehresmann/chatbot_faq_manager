c4c429ab1f1fc04b87369d8f2be1266ca026d55c
rodrigo-ehresmann <igoehresmann@gmail.com>
2017-05-27 16:13:43 -0300

    Fix load bug of Faq::Destroyer

--- app.rb
+++ app.rb
@@ -4,9 +4,10 @@ require 'sinatra/activerecord'
 
 require './config/database'
 
-Dir["./app/models/*.rb"].each {|file| require file }
-Dir["./app/services/**/*.rb"].each {|file| require file }
-Dir["./app/classes/**/*.rb"].each {|file| require file }
+Dir["./app/models/*.rb"].each { |file| require file }
+Dir["./app/services/**/*.rb"].each { |file| require file }
+require './app/classes/faq/utils.rb'
+Dir["./app/classes/**/*.rb"].each { |file| require file }
 
 
 class App < Sinatra::Base
