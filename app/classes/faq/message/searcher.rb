ab4ad658c86880020b306885a18b1c86e8f85875
rodrigo-ehresmann <igoehresmann@gmail.com>
2017-05-27 22:02:50 -0300

    Added faq messages classes

---
+++ app/classes/faq/message/searcher.rb
@@ -0,0 +1,43 @@
+module Faq
+  module Message
+
+    class Searcher
+      def initialize(args)
+        @success = args[:success]
+        @info = args[:info]
+      end
+
+      def show
+        if @success
+          success_message
+        else
+          error_message
+        end
+      end
+
+      private
+
+      def success_message
+        message =  "Check what I found in our *FAQ*:\n\n"
+
+        @info[:questions].each do |question|
+          message += "*#{question.id} - #{f.question}\n"
+          question.answers.each { |answer|
+            message += "> #{answer.id} - #{answer.content}\n"
+          }
+          question.tags.each { |tag| message += "_##{tag}_ "}
+        end
+      end
+
+      def error_message
+        case @info[:error]
+        when :search
+          "Sorry, I couldn't understand what you want to search."
+        else
+          "Something wrong happened."
+        end
+      end
+    end
+
+  end
+end
