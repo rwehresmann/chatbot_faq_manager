cc0f9a96c659f5a0ad2d8aea4b7cbf5cd132e4ce
rodrigo-ehresmann <igoehresmann@gmail.com>
2017-05-29 00:01:49 -0300

    Added search_type method to Utils and created its tests

--- spec/classes/utils_spec.rb
+++ spec/classes/utils_spec.rb
@@ -12,4 +12,12 @@ describe Utils do
       it { expect(object.split_tags("ruby, linux")).to eq ["ruby", "linux"] }
     end
   end
+
+  describe '#search_type' do
+    it "returns always the last piece from a string separated by '_'" do
+      ["faq_search_by_term, link_search_all"].each { |string|
+        expect(object.search_type(string)).to eq string.split("_").last
+      }
+    end
+  end
 end
