43c860200d1ecf7f25731ecc739a86d25af1613a
rodrigo-ehresmann <igoehresmann@gmail.com>
2017-05-29 21:05:43 -0300

    Made webhook send a symbolized hash to Interpreter

--- app.rb
+++ app.rb
@@ -21,14 +21,14 @@ class App < Sinatra::Base
   # Example of how to test with curl:
   # curl -H "Content-Type: application/json" -X POST -d '{ "result": { "source": "agent", "resolvedQuery": "help", "action": "help", "actionIncomplete": false, "parameters": {}, "contexts": [], "metadata": { "intentId": "3314997b-0428-4efb-808c-f3708f059a85", "webhookUsed": "true", "webhookForSlotFillingUsed": "false", "webhookResponseTime": 36, "intentName": "help" } } }' http://localhost:9292/webhook
   post '/webhook' do
-    result = JSON.parse(request.body.read)["result"]
+    result = JSON.parse(request.body.read, symbolize_names: true)[:result]
     if result["contexts"].present?
       response = Faq::InterpretService.call(
-        result["action"],
-        result["contexts"][0]["parameters"]
+        result[:action],
+        result[:contexts][0][:parameters]
       )
     else
-      response = Faq::InterpreterService.call(result["action"], result["parameters"])
+      response = Faq::InterpreterService.call(result[:action], result[:parameters])
     end
 
     content_type :json
